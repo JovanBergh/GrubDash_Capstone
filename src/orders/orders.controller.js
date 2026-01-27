f85fe8591f6fa786940b8923462692528bfde557
Jovan Bergh <jovanbergh17@gmail.com>
2026-01-27 04:16:46 -0700

    Added error handling for invalid methods and req.body property validation

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -1,18 +1,43 @@
 const path = require("path");
 
+const valid = require("../errors/validProperty");
+
 // Use the existing order data
 const orders = require(path.resolve("src/data/orders-data"));
 
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-
 //Method: List
 function list(req, res) {
-    res.json({ data: orders });
+  res.json({ data: orders });
+}
+
+//Validate: populated dishes array included
+function dishOrder(req, res, next) {
+  const { data: { dishes } = {} } = req.body;
+  if (!dishes) {
+    return next({
+      status: 400,
+      message: "Order must include a dish",
+    });
+  }
+  if (Array.isArray(dishes) && dishes.length > 0) {
+    return next();
+  }
+  return next({
+    status: 400,
+    message: "Order must include at least one dish"
+  })
+}
+
+//Validate: quantity
+function quantity(req, res, next) {
+
 }
 
 //Export
 module.exports = {
-    list,
-}
+  list,
+  create: [valid("deliverTo"), valid("mobileNumber"), dishOrder],
+};
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const controller = require("./orders.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed")
 
 //Route: Main
-router.route("/").get(controller.list);
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
+
+//router.route("/:orderId").all(methodNotAllowed);
 
 module.exports = router;
