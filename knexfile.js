8dce7f99e950018c2f1e328c678f61b81d71ac98
Jovan Bergh <jovanbergh17@gmail.com>
2026-03-13 11:08:18 -0600

    Corrected knexfile for production

--- knexfile.js
+++ knexfile.js
@@ -38,18 +38,16 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: __dirname + "/src/db/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/src/db/seeds",
+    },
   }
-
 };
